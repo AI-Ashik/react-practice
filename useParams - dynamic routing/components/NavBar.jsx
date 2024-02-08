36f721de8c08d066da6a6526618f57824d44bddf
AI-Ashik <arifulislamashik07@gmail.com>
2024-02-08 23:27:47 +0600

    added styling to navbar

--- useParams - dynamic routing/components/NavBar.jsx
+++ useParams - dynamic routing/components/NavBar.jsx
@@ -1,14 +1,27 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import styles from "./styles/NavBar.module.css";
 
 const NavBar = () => {
   return (
     <div>
-      <nav>
-        <ul>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/contact">Contact</NavLink>
-          <NavLink to="/blogs">Blogs</NavLink>
+      <nav className={styles.nav}>
+        <ul className={styles.navLinks}>
+          <li>
+            <NavLink to="/" className={styles.activeLink}>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/contact" className={styles.activeLink}>
+              Contact
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/blogs" className={styles.activeLink}>
+              Blogs
+            </NavLink>
+          </li>
         </ul>
       </nav>
     </div>
