bb7d4a28a5d05f73cb61376b3d9209203511d1f7
AI-Ashik <arifulislamashik07@gmail.com>
2024-02-08 23:38:24 +0600

    created dummyData and showed the data from blog page

---
+++ useParams - dynamic routing/src/data.jsx
@@ -0,0 +1,18 @@
+export const data = [
+  {
+    id: 1,
+    title: "First Post",
+    body: "This is the body of the first post. Lorem ipsum dolor sit amet, consectetur adipiscing elit.This is the body of the third post. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    id: 2,
+    title: "Second Post",
+    body: "This is the body of the second post. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.This is the body of the third post. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    id: 3,
+    title: "Third Post",
+    body: "This is the body of the third post. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.This is the body of the third post. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  // Add more objects as needed
+];
--- useParams - dynamic routing/src/pages/Blogs.jsx
+++ useParams - dynamic routing/src/pages/Blogs.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { data } from "../data";
 
 const Blogs = () => {
-  return <div>Blogs</div>;
+  const [blogs, setBlogs] = useState(data);
+  return (
+    <div>
+      <h2 style={{ textAlign: "center" }}>Blogs Page</h2>
+
+      <div>
+        {blogs.map((blog) => {
+          const { id, title, body } = blog;
+          return (
+            <article style={{ margin: "20px 5px" }}>
+              <h2>{title}</h2>
+              <p>{body}</p>
+            </article>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Blogs;
