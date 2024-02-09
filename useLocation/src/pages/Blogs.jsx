c8c27290551f25a4259f44820b96fb89f4e0b115
AI-Ashik <arifulislamashik07@gmail.com>
2024-02-09 23:39:34 +0600

    get data from api

--- useLocation/src/pages/Blogs.jsx
+++ useLocation/src/pages/Blogs.jsx
@@ -1,10 +1,24 @@
-// Blogs Component
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { data } from "../data";
 
 const Blogs = () => {
-  const [blogs, setBlogs] = useState(data);
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const fetchPosts = async () => {
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/posts"
+      );
+      const data = await response.json();
+      setPosts(data);
+    } catch (error) {
+      console.error("Error fetching posts:", error);
+    }
+  };
 
   const truncateString = (str, num) => {
     if (str.length > num) {
@@ -19,18 +33,21 @@ const Blogs = () => {
       <h2 style={{ textAlign: "center" }}>Blogs Page</h2>
 
       <div>
-        {blogs.map((blog) => {
-          const { id, title, body } = blog;
-          return (
-            <article style={{ margin: "20px 5px" }} key={id}>
-              <h2>{title}</h2>
-              <p>{truncateString(body, 100)}</p>
-              <Link to={`/blogs/${title}`} state={{ title, body }}>
-                Learn More
-              </Link>
-            </article>
-          );
-        })}
+        <h1>Posts</h1>
+        <ul>
+          {posts.map((post) => {
+            const { id, title, body } = post; // Destructure post object here
+            return (
+              <li key={id}>
+                <h2>{truncateString(title, 50)}</h2>
+                <p>{truncateString(body, 50)}</p>
+                <Link to={title} state={{ title, body }}>
+                  Learn More...
+                </Link>{" "}
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </div>
   );
