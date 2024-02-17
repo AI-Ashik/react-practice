7533a045c62276aefa89d6c5a900ec8e6a2752c4
AI-Ashik <arifulislamashik07@gmail.com>
2024-02-17 11:08:39 +0600

    setup protected route

--- protectedRoute/src/App.jsx
+++ protectedRoute/src/App.jsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import NavBar from "../components/NavBar";
+import Protected from "./Protected";
 import AddBlog from "./pages/AddBlog";
 import Blogs from "./pages/Blogs";
 import Contact from "./pages/Contact";
@@ -7,14 +9,34 @@ import Error from "./pages/Error";
 import Home from "./pages/Home";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleClick = () => {
+    setIsLoggedIn(!isLoggedIn);
+  };
+
   return (
     <>
       <NavBar />
+
+      {isLoggedIn ? (
+        <button onClick={handleClick}>Login</button>
+      ) : (
+        <button onClick={handleClick}>Log Out</button>
+      )}
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/blogs" element={<Blogs />} />
-        <Route path="/add-blog" element={<AddBlog />} />
+        <Route
+          path="/add-blog"
+          element={
+            <Protected isLoggedIn={isLoggedIn}>
+              <AddBlog />
+            </Protected>
+          }
+        />
         <Route path="*" element={<Error />} />
       </Routes>
     </>
