d44f76a9e636cf749a7e4f1b798bd6963443214e
AI-Ashik <arifulislamashik07@gmail.com>
2024-02-22 16:01:48 +0600

    created json server and used it by fetched data using axios.get method

--- JSON Server/src/App.jsx
+++ JSON Server/src/App.jsx
@@ -1,7 +1,29 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function App() {
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    const response = await axios.get("http://localhost:3001/posts");
+    const data = response.data;
+    setData(data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
+
   return (
     <>
-      <h2>Hello World</h2>
+      <h2>Data </h2>
+
+      {data.map((data) => (
+        <div key={data.id}>
+          <h3>{data.title}</h3>
+          <p>{data.views}</p>
+        </div>
+      ))}
     </>
   );
 }
